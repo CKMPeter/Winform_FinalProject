d1989d4eae1b57e53f46934fa89e046729c48abb
CKMPeter <126633396+CKMPeter@users.noreply.github.com>
2024-04-10 18:37:35 +0700

    update Cart

--- FinalProject_WinForm/Queries/TradeApp.sql
+++ FinalProject_WinForm/Queries/TradeApp.sql
@@ -1,6 +1,10 @@
 ﻿drop table UserInfo;
 drop table ItemInfo;
+drop procedure AddToCartProc;
+drop table CartDetail;
+drop view CartView;
 
+--Create tables
 create table UserInfo(
 	UserName VarChar(50) Primary Key,
 	UserPassword VarChar(50) not null,
@@ -13,6 +17,7 @@ create table UserInfo(
 go
 
 create table ItemInfo(
+	ItemId uniqueidentifier default newid() primary key,
 	ItemName VarChar(50),
 	ItemQuantity int,
 	ItemDiscription VarChar(100),
@@ -22,14 +27,25 @@ create table ItemInfo(
 	ItemImage2 VarChar(max),
 	ItemYear int,
 	ItemQuanlity int,
-	UserName VarChar(50) References UserInfo(UserName),
+	OwnerName VarChar(50) References UserInfo(UserName),
 	ItemOldPrice int,
-	ItemRating int,
-	Primary Key (ItemName, UserName)
+	ItemRating int
 );
 go
 
-create procedure UpdateRating
+
+
+create table CartDetail(
+	ItemId uniqueidentifier references ItemInfo(ItemId),
+	UserName VarChar(50) references UserInfo(UserName),
+	ItemQuantity int,
+	Constraint PK_CartDetail primary key (ItemId, UserName),
+	IsPay BIT default 0
+);
+go
+
+--Procedures
+create procedure UpdateRatingProc
 	@ItemName VarChar(50),
 	@ItemRating int
 as
@@ -40,7 +56,27 @@ begin
 end;
 go
 
+create procedure AddToCartProc
+	@UserName Varchar(50),
+	@ItemId uniqueidentifier,
+	@ItemQuantity int
+as
+begin
+	Insert into CartDetail(ItemId, UserName, ItemQuantity) Values (@ItemId, @UserName, @ItemQuantity)
+end;
+go
+
+--Views
+create view CartView as
+select i.ItemName, i.ItemPrice, cd.UserName, cd.ItemId, cd.ItemQuantity
+from CartDetail cd
+join UserInfo u on cd.UserName = u.UserName
+join ItemInfo i on cd.ItemId = i.ItemId
+go
+
+
+--View Table
 select * from UserInfo;
 select * from ItemInfo;
-
---delete from ItemInfo where ItemName = '1234';
+select * from CartDetail;
+select * from CartView;
